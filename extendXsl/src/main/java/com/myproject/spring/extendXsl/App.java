c565238738472b08ad5705015c11672b3adde2e7
ak761566 <41939307+ak761566@users.noreply.github.com>
2021-05-31 22:54:20 +0530

    First commit of innodata project MLApi. It is implementation of ML service develop by innodata lab into java for XSLT.

---
+++ extendXsl/src/main/java/com/myproject/spring/extendXsl/App.java
@@ -0,0 +1,27 @@
+package com.myproject.spring.extendXsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+//@SpringBootApplication
+//@ComponentScan
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( args[1] + " Test!" );
+    	//ApplicationContext context = SpringApplication.run(App.class, args);
+    	//testClass test = context.getBean(testClass.class);
+    	//MLApiClient client = context.getBean(MLApiClient.class);
+    	//System.out.println(testClass.post_badBibData("Centers for Disease Control &amp; Prevention. Viral Hepatitis-Hepatitis C Information [Internet]. 2016. Available from: http://www.cdc.gov/hepatitis/hcv/hcvfaq.htm. Cited 9 Jun 2016."));
+    	//System.out.println(testClass.postHttpClient());
+    	
+    	//System.out.println(testClass.testMe(10, 20));
+    	//System.out.println(testClass.getClassName());
+    	
+    	//System.out.println(MLApiClient.post_badNoteData("Chair of Econophysics &amp; Complex Systems, Ecole polytechnique, 91128 Palaiseau Cedex, France"));
+    	//System.out.println(MLApiClient.post_badNoteData("Institut de Recherche en Mathématique et Physique, Université catholique de Louvain, Louvain-la-Neuve, B-1348, Belgium"));
+    }
+}
